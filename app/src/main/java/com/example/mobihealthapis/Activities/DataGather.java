d873d0094b454bafbf97dce45e3517ea76f228d6
Swapnil <swapnilshah5889@gmail.com>
2020-01-28 12:33:33 +0530

    added data gathering activity

---
+++ app/src/main/java/com/example/mobihealthapis/Activities/DataGather.java
@@ -0,0 +1,14 @@
+package com.example.mobihealthapis;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataGather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_gather);
+    }
+}
---
+++ app/src/main/java/com/example/mobihealthapis/Adapters/DataMapAdapter.java
@@ -0,0 +1,4 @@
+package com.example.mobihealthapis.Adapters;
+
+public class DataMapAdapter {
+}
